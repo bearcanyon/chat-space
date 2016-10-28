055152a8f5adcb6903fabec5b2479feca2a03f31
KumagaiNaoki <j.a.r.n0405@gmail.com>
2016-10-29 03:42:48 +0900

    Add_index_migrate
    # What
    migrationファイルに
    indexを貼った。
    :messages, :group_id<!-- これはすでに貼ってあるって言われた -->
    :users, :name
    :users, :email, unique: true
    <!--これはdeviseですでにある-->
    :group :name
    :users_groups, [:group_id, :user_id]
    <!--結果この二つだけ貼った-->

---
+++ db/migrate/20161028183452_add_name_to_groups.rb
@@ -0,0 +1,5 @@
+class AddNameToGroups < ActiveRecord::Migration[5.0]
+  def change
+  	add_index :groups, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028182400) do
+ActiveRecord::Schema.define(version: 20161028183653) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name", using: :btree
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -52,6 +53,7 @@ ActiveRecord::Schema.define(version: 20161028182400) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_users_groups_on_group_id", using: :btree
+    t.index ["user_id", "group_id"], name: "index_users_groups_on_user_id_and_group_id", using: :btree
     t.index ["user_id"], name: "index_users_groups_on_user_id", using: :btree
   end
 
