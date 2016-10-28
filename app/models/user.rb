d8a07966e9344fcd749aea35411122d915efc106
KumagaiNaoki <j.a.r.n0405@gmail.com>
2016-10-29 03:55:39 +0900

    Add_Association
    # What
    Associationの記述をした。
    
    # Why
    データベース設計を終わらせて寝るため。
    なんのためかと聞かれたら寝るため。
    ただひたすら寝たかった。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :messages
+  has_many :users_groups
+  has_many :groups, through: users_groups
 end
