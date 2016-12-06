b8f07b53990abb416d56ad93ef4abc1c5e541d61
KumagaiNaoki <j.a.r.n0405@gmail.com>
2016-12-07 01:38:42 +0900

    fix

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,9 +4,12 @@ class GroupsController < ApplicationController
 	end
 	def create
 		@group = Group.new(create_params)
-		@group.save
+		if @group.save
+			redirect_to :root
+		else
+		end
 	end
 	def create_params
-		params.require(:group).permit(:name)
+		params.require(:group).permit(:name, user_ids: [])
 	end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
 	has_many :messages
 	has_many :users_groups
 	has_many :users, through: :users_groups
+	accepts_nested_attributes_for :users
 end
