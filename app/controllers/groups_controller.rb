0ba0e5c2ac3180e5a7dae0c324dc352924cb528e
KumagaiNaoki <j.a.r.n0405@gmail.com>
2016-12-06 23:10:57 +0900

    グループの名前だけ保存できた

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,12 @@
 class GroupsController < ApplicationController
+	def new
+		@group = Group.new
+	end
+	def create
+		@group = Group.new(create_params)
+		@group.save
+	end
+	def create_params
+		params.require(:group).permit(:name)
+	end
 end
